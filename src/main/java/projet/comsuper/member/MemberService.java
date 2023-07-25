382b5263a4252bbd64a3127c37263b7ce7070bb4
Kownsora <cielo7824@gmail.com>
2023-07-26 04:41:22 +0900

    테스트 소스

---
+++ src/main/java/projet/comsuper/member/MemberService.java
@@ -0,0 +1,51 @@
+package projet.comsuper.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복회원 체크
+     */
+    private void validateDuplicateMember(Member member) {
+        //Optional<Member> result = memberRepository.findByName(member.getName()); 아래와 같은 의미
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+
+    }
+
+}
