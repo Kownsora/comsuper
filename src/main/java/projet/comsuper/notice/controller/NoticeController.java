05c4966149f9372fc1e3e5e1aa8f43b18fc06ef8
Kownsora <cielo7824@gmail.com>
2023-07-28 03:15:15 +0900

    공지시항 백엔드

--- src/main/java/projet/comsuper/notice/controller/NoticeController.java
+++ src/main/java/projet/comsuper/notice/controller/NoticeController.java
@@ -1,4 +1,4 @@
-package projet.comsuper.notice;
+package projet.comsuper.notice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
